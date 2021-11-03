48f2dbc5887241075b83e383bb06c5f74620f32a
smh1988-2 <seamhurley1988@gmail.com>
2021-11-02 17:56:44 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+//function expession that returns divides 2000 by 100
+const divide = function(num1 = 2000, num2 = 100) {
+    return num1 / num2
+};
+divide;
+
+const divide2 = (num1, num2) => num1 / num2; 
+console.log(divide2(10,2));
+
+const square = (num) => num * num;
+console.log(square(5));
+
+const add = (num1, num2) => num1 + num2;
+console.log(add(5,5));
